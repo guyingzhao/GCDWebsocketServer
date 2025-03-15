aeb0fb3a053003c50006cc0f42db8128fd31b420
guyingzhao <572488191@qq.com>
2025-03-15 15:51:48 +0800

    init pod lib

---
+++ Example/Pods/Target Support Files/GCDWebsocketServer/GCDWebsocketServer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GCDWebsocketServer : NSObject
+@end
+@implementation PodsDummy_GCDWebsocketServer
+@end
---
+++ Example/Pods/Target Support Files/Pods-GCDWebsocketServer_Example/Pods-GCDWebsocketServer_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GCDWebsocketServer_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GCDWebsocketServer_Example
+@end
